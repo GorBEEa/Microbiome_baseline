9bac27178d88977a4564d74c20827eed8ae9c6b4
Luisja Chueca <luisjavier.chueca@bc3research.org>
2025-08-01 14:08:47 +0200

    upadating 03_PICRUSt2.sh script

--- scripts/03_PICRUSt2.sh
+++ scripts/03_PICRUSt2.sh
@@ -18,13 +18,16 @@ FASTA_FILE="${DATA_DIR}/2023_16S_GorBEEa_prj_ASVs_filt_c.${Th}.fa"
 OUTPUT_DIR="${DATA_DIR}/picrust2_output_${Th}"
 CPU=4
 
-# Create output directory if it doesn't exist
-mkdir -p "${OUTPUT_DIR}"
-
-# Define log and error files inside the output directory
-LOG_FILE="${OUTPUT_DIR}/picrust2_${Th}.log"
-ERR_FILE="${OUTPUT_DIR}/picrust2_${Th}.err"
+# Define log and error files inside a logs subdirectory within the output directory
+LOG_DIR="./logs"
+mkdir -p "${LOG_DIR}"
+LOG_FILE="${LOG_DIR}/picrust2_${Th}.log"
+ERR_FILE="${LOG_DIR}/picrust2_${Th}.err"
 
 # Run PICRUSt2 pipeline
-picrust2_pipeline.py -s "${FASTA_FILE}" -i "${ASV_TABLE}" -o "${OUTPUT_DIR}" -p "${CPU}" > "${LOG_FILE}" 2> "${ERR_FILE}"
+picrust2_pipeline.py \
+	-s "${FASTA_FILE}" -i "${ASV_TABLE}" -o "${OUTPUT_DIR}" \
+	-p "${CPU}" \
+	--per_sequence_contrib \
+	> "${LOG_FILE}" 2> "${ERR_FILE}"
 
